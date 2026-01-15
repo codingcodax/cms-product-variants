15479107932c4601c108f6ac84bef783db61261e
Alexis Guzman <codingcodax@gmail.com>
2026-01-15 13:49:39 -0600

    feat(fields): add basic

---
+++ src/fields/link-group.ts
@@ -0,0 +1,26 @@
+import type { ArrayField, Field } from 'payload';
+import { deepMerge } from '@/lib/deep-merge';
+import type { LinkAppearances } from './link';
+import { link } from './link';
+
+type LinkGroupType = (options?: {
+	appearances?: LinkAppearances[] | false;
+	overrides?: Partial<ArrayField>;
+}) => Field;
+
+export const linkGroup: LinkGroupType = ({
+	appearances,
+	overrides = {},
+} = {}) => {
+	const generatedLinkGroup: Field = {
+		name: 'links',
+		type: 'array',
+		fields: [
+			link({
+				appearances,
+			}),
+		],
+	};
+
+	return deepMerge(generatedLinkGroup, overrides);
+};
