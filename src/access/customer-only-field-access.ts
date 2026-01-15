30c3c305cb740045a2d25b093ab9e4efcc980c0a
Alexis Guzman <codingcodax@gmail.com>
2026-01-15 13:44:30 -0600

    feat(payload): create access functions

---
+++ src/access/customer-only-field-access.ts
@@ -0,0 +1,11 @@
+import type { FieldAccess } from 'payload';
+
+import { checkRole } from '@/access/utilities';
+
+export const customerOnlyFieldAccess: FieldAccess = ({ req: { user } }) => {
+	if (user) {
+		return checkRole(['customer'], user);
+	}
+
+	return false;
+};
