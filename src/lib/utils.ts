f5d8360c16e1892dd60867a3663338897592944f
Alexis Guzman <codingcodax@gmail.com>
2026-01-15 13:49:18 -0600

    feat(lib): add `capitalize-first-letter`  function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
 	return twMerge(clsx(inputs));
 };
+
+export const capitaliseFirstLetter = (str: string): string => {
+	if (!str) {
+		return str;
+	}
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
