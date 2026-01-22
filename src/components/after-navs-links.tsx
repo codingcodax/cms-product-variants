f872c850c0622b78e84140387f009e6bbfb0a8a7
Alexis Guzman <codingcodax@gmail.com>
2026-01-22 17:02:37 -0600

    feat(components): hide links if no admin role

--- src/components/after-navs-links.tsx
+++ src/components/after-navs-links.tsx
@@ -1,6 +1,14 @@
-import { Link, NavGroup } from '@payloadcms/ui';
+'use client';
+
+import { Link, NavGroup, useAuth } from '@payloadcms/ui';
 
 export const AfterNavsLinks = () => {
+	const { user } = useAuth();
+
+	if (!user?.roles?.includes('admin')) {
+		return null;
+	}
+
 	return (
 		<NavGroup label='Back-office'>
 			<Link className='nav__link' href='/admin/back-1'>
